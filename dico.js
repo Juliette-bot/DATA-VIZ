1b3ae371a706bae0c3590d8ef5051de2ed0d1bf6
Patrice976 <catinatp@gmail.com>
2025-07-23 14:31:34 +0200

    first commit

---
+++ dico.js
@@ -0,0 +1,60 @@
+export const citaionStreameuses = 
+
+[
+    {
+        "name" : "Mina",
+        "citation" : "On fait tous des erreurs, mais ça ne nous définit pas. Ce qui compte, c’est la manière dont on se relève"
+    },
+
+    {
+        "name" : "Mademoiselle Pousse",
+        "citation" : "Les trolls existent, mais on les laisse parler dans le vide. Moi, je préfère écouter les gens qui m'apportent de l'amour."
+    },
+
+    {
+        "name" : "Plumita",
+        "citation" : "La vraie magie, c’est quand tout le monde se sent à sa place ici"
+    },
+
+    {
+        "name" : "MissJirachi",
+        "citation" : "On est plus fortes ensemble. C’est ça le vrai pouvoir."
+    },
+
+     {
+        "name" : "Moka",
+        "citation" : "On vit dans un monde où il faut prouver sa valeur, mais parfois c’est juste en étant soi-même qu’on est le plus fort."
+    },
+
+
+     {
+        "name" : "MlleSunny",
+        "citation" : "On ne va pas s’excuser d’exister. Si on est là, c’est parce qu’on mérite notre place."
+    },
+
+     {
+        "name" : "Léa",
+        "citation" : "Il n'y a rien de plus important que de prendre soin de soi, mentalement et physiquement."
+    },
+
+     {
+        "name" : "Maghla",
+        "citation" : "Je ne suis pas une poupée gonflable, je suis une streameuse. Respectez ça."
+    },
+
+     {
+        "name" : "Clara_C_Moi",
+        "citation" : "Là, tu vois, je suis pas en train de gamer, je suis en train de sauver le monde, tranquillement."
+    },
+
+     {
+        "name" : "Clara_C_Moi ",
+        "citation" : "Je fais pas de la game, je fais de l'art... Ou pas, mais ça m'arrange"
+    },
+
+     {
+        "name" : "Clara_C_Moi",
+        "citation" : "Je suis une streameuse, donc je suis une pro... Bon, je suis une pro de la rigolade"
+    }
+
+]
---
+++ script.js
@@ -0,0 +1,172 @@
+import { citaionStreameuses } from './dico.js'
+
+const btnReset = document.getElementById('btnReset')
+const input = document.getElementById('input')
+const divInfo1 = document.getElementById('divInfo1')
+
+setTimeout(() => {
+
+    btnResetAndInput()
+    clearCitations()
+
+}
+    , 3000)
+
+
+function hideBtnResteAndInput() {
+    const input = document.getElementById('input')
+    const btnReset = document.getElementById('btnReset')
+
+    input.style.display = 'none'
+    btnReset.style.display = "none"
+}
+
+hideBtnResteAndInput()
+
+
+function citationAleatoirStreameuses() {
+    let ciationAleatoire = Math.floor(Math.random() * citaionStreameuses.length)
+    let citationAleatoireValue = citaionStreameuses[ciationAleatoire]
+    console.log(citationAleatoireValue)
+
+    const h1citationsStreameuse = document.createElement("h1")
+    h1citationsStreameuse.innerHTML = citationAleatoireValue.citation
+    h1citationsStreameuse.classList.add('citations')
+
+    const pNameStreameuse = document.createElement("p")
+    pNameStreameuse.innerHTML = citationAleatoireValue.name
+    pNameStreameuse.classList.add('nameStreameuse')
+
+    divLaoding.appendChild(h1citationsStreameuse)
+    divLaoding.appendChild(pNameStreameuse)
+
+
+}
+citationAleatoirStreameuses()
+
+
+function clearCitations() {
+
+    const divLaoding = document.getElementById('divLaoding')
+
+    divLaoding.style.display = "none"
+}
+
+
+function btnResetAndInput() {
+    const input = document.getElementById('input')
+    const btnReset = document.getElementById('btnReset')
+
+    input.style.display = 'flex'
+    btnReset.style.display = "flex"
+
+}
+
+
+input.addEventListener('keydown', async (event) => {
+    if (event.key === 'Enter' && event.target.value !== '') {
+
+        const response = await fetchTwitch(event.target.value)
+        console.log(response.followers_total)
+        const pFollowers = document.createElement('p')
+        pFollowers.innerText = response.followers_total
+        divInfo1.appendChild(pFollowers)
+
+        etoilesContent()
+        anyme023Content()
+        squeezieContent()
+
+
+
+    }
+
+})
+
+async function fetchTwitch(searchStreameuse) {
+    const response = await fetch(`https://twitchtracker.com/api/channels/summary/${searchStreameuse}`)
+    const streaumeuse = await response.json()
+    return streaumeuse
+
+
+}
+
+
+async function etoilesContent() {
+
+    const response = await fetch(`https://twitchtracker.com/api/channels/summary/etoiles`)
+    const data = await response.json();
+
+    const pEtoilesFollowers = document.createElement('p')
+    pEtoilesFollowers.innerText = data.followers_total
+    divInfo1.appendChild(pEtoilesFollowers)
+    pEtoilesFollowers.classList.add('etoilesFollowers')
+
+
+    const pEtoilesHours = document.createElement('p')
+    pEtoilesHours.innerText = data.hours_watched
+    divInfo1.appendChild(pEtoilesHours)
+    pEtoilesHours.classList.add('etoilesHours')
+
+    const pEtoilesRank = document.createElement('p')
+    pEtoilesRank.innerText = data.rank
+    divInfo1.appendChild(pEtoilesRank)
+    pEtoilesRank.classList.add('etoilesRank')
+
+
+}
+
+
+
+async function anyme023Content() {
+
+    const response = await fetch(`https://twitchtracker.com/api/channels/summary/anyme023`)
+    const data = await response.json();
+
+
+    const pAnymeFollowers = document.createElement('p')
+    pAnymeFollowers.innerText = data.followers_total
+    divInfo1.appendChild(pAnymeFollowers)
+    pAnymeFollowers.classList.add('anymeFollowers')
+
+
+    const pAnymeHours = document.createElement('p')
+    pAnymeHours.innerText = data.hours_watched
+    divInfo1.appendChild(pAnymeHours)
+    pAnymeHours.classList.add('anymeHours')
+
+    const pAnymeRank = document.createElement('p')
+    pAnymeRank.innerText = data.rank
+    divInfo1.appendChild(pAnymeRank)
+    pAnymeRank.classList.add('anymeRank')
+
+}
+
+
+
+async function squeezieContent() {
+    const response = await fetch(`https://twitchtracker.com/api/channels/summary/squeezie`)
+    const data = await response.json()
+
+    const pSqueezieFollowers = document.createElement('p')
+    pSqueezieFollowers.innerText = data.followers_total
+    divInfo1.appendChild(pSqueezieFollowers)
+    pSqueezieFollowers.classList.add('squeezieFollowers')
+
+
+    const pSqueezieHours = document.createElement('p')
+    pSqueezieHours.innerText = data.hours_watched
+    divInfo1.appendChild(pSqueezieHours)
+    pSqueezieHours.classList.add('squeezieHours')
+
+    const pSqueezieRank = document.createElement('p')
+    pSqueezieRank.innerText = data.rank
+    divInfo1.appendChild(pSqueezieRank)
+    pSqueezieRank.classList.add('squeezieRank')
+
+
+}
+
+
+
+
+
